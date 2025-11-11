47455b6f0756ddc8b3816f9d6212b2df2849a645
princla-dev <princla.mkt@gmail.com>
2025-11-11 11:33:20 -0300

    Configure Supabase project credentials

---
+++ config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://petvbrlseijabedxaspz.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldHZicmxzZWlqYWJlZHhhc3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NjIxNzYsImV4cCI6MjA3ODQzODE3Nn0.bRIv13iu82ukhzY7g5X55oDRiUcGVg0tqcNUvd135xg";
